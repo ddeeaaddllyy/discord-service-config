32d20be4ce44db34d5d4a2cb768d46a84a57a0f9
ddeeaaddllyy <naumova070783@gmail.com>
2026-02-21 21:19:05 +0300

    initial commit

---
+++ iint/__init__.py
@@ -0,0 +1,3 @@
+from .core import iint
+
+__all__ = ['iint']
---
+++ tests/normal_test.py
@@ -0,0 +1,12 @@
+from iint import iint
+
+i = iint("77")
+y = iint(77)
+print()
+
+print(hex(y.to_int()))
+print((80 and "6"))
+
+for l in i:
+    print(f"1. {type(l + 2 + 'w')}, 2. {l + 2 + 'w'}")
+
